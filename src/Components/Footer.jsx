a4601df973e5c159629d38375f3162789c563923
ku820780 <ku820780@gmail.com>
2024-01-17 15:38:18 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+// import { useAVToggle } from '@100mslive/react-sdk'
+
+// function Footer() {
+//     const {isLocalAudioEnabled, toggleAudio, isLocalVideoEnabled, toggleVideo} = useAVToggle;
+
+//   return (
+//     <div className='control-bar'>
+//         <button className='btn-control' onClick={toggleAudio}>
+//             {isLocalAudioEnabled ? "Mute" : "Unmute"}
+//         </button>
+//         <button className='btn-control' onClick={toggleVideo}>
+//             {isLocalVideoEnabled ? "Hide" : "Unhide"}
+//         </button>
+//     </div>
+//   )
+// }
+
+// export default Footer
+
+import { useAVToggle } from "@100mslive/react-sdk";
+
+const Footer = () => {
+  const { isLocalAudioEnabled, toggleAudio, isLocalVideoEnabled, toggleVideo } =
+    useAVToggle();
+  return (
+    <div className="control-bar">
+      <button className="btn-control" onClick={toggleAudio}>
+        {isLocalAudioEnabled ? "Mute" : "Unmute"}
+      </button>
+      <button className="btn-control" onClick={toggleVideo}>
+        {isLocalVideoEnabled ? "Hide" : "Unhide"}
+      </button>
+    </div>
+  );
+};
+export default Footer;
